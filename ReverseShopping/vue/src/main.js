768c333fd6ee8efa79922889283857e8386bd4e7
toshiaki.suzuki <toshiaki_suzuki@kogasoftware.com>
2020-02-07 19:08:52 +0900

    axiosをインストール

--- ReverseShopping/vue/src/main.js
+++ ReverseShopping/vue/src/main.js
@@ -5,9 +5,12 @@ import store from './store'
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
+Vue.use(VueAxios, axios) 
 
 Vue.config.productionTip = false
 
