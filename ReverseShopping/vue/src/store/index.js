793db4ac98f78d376ad9f642207c617f7c07ed35
toshiaki.suzuki <toshiaki_suzuki@kogasoftware.com>
2020-02-08 10:27:15 +0900

    ユーザー情報画面作成

--- ReverseShopping/vue/src/store/index.js
+++ ReverseShopping/vue/src/store/index.js
@@ -9,32 +9,42 @@ export default new Vuex.Store({
     needsDataList : [
       {
         'id': '1',
-        'name': 'good1',
+        'name': 'need1',
         'price': 10000,
         'number': '1',
         'deadline': '2020/3/31',
-        'description': 'good1です。',
+        'description': 'need1です。',
         'image': require('../assets/image001.jpg')
       },
       {
         'id': '2',
-        'name': 'good2',
+        'name': 'need2',
         'price': 20000,
         'number': '1',
         'deadline': '2020/3/31',
-        'description': 'good2です。',
+        'description': 'need2です。',
         'image': require('../assets/image002.jpg')
       },
       {
         'id': '3',
-        'name': 'good3',
+        'name': 'need3',
         'price': 30000,
         'number': '1',
         'deadline': '2020/3/31',
-        'description': 'good3です。',
+        'description': 'need3です。',
         'image': require('../assets/image003.jpg')
       },
-    ]
+    ],
+    loginUserData: {
+        'id': '1',
+        'name': 'hoge',
+        'nickname': 'fuga',
+        'address': '東京都台東区上野1-17-6',
+        'tel': '03-3833-0733',
+        'email': 'asdjdahaj@aksj.jp',
+        'settlement_info': '*************',
+        'image': require('../assets/user_no_image.jpg')
+    }
   },
   mutations: {
     signIn(state) {
--- ReverseShopping/vue/src/views/User.vue
+++ ReverseShopping/vue/src/views/User.vue
@@ -1,5 +1,59 @@
 <template>
   <div class="user">
-    <h1>ユーザー情報画面</h1>
+    <h2>ユーザー情報</h2>
+    <img class="img-thumbnail rounded mt-4" :src="userData.image">
+    <table class="table mx-auto mt-4" style="width: 50%">
+      <tr>
+        <th>氏名</th>
+        <td>{{ userData.name }}</td>
+      </tr>
+      <tr>
+        <th>ニックネーム</th>
+        <td>{{ userData.nickname }}</td>
+      </tr>
+      <tr>
+        <th>住所</th>
+        <td>{{ userData.address }}</td>
+      </tr>
+      <tr>
+        <th>TEL</th>
+        <td>{{ userData.tel }}</td>
+      </tr>
+      <tr>
+        <th>メールアドレス</th>
+        <td>{{ userData.email }}</td>
+      </tr>
+      <tr>
+        <th>決済情報</th>
+        <td>{{ userData.settlement_info }}</td>
+      </tr>
+    </table>
+    <h2>募集中一覧</h2>
+    <NeedsList></NeedsList>
   </div>
 </template>
+
+<script>
+import NeedsList from "../components/NeedsList.vue"
+
+export default {
+  name: 'User',
+  components: { NeedsList },
+  data() {
+    return {
+      userData: this.$store.state.loginUserData
+    }
+  }
+}
+</script>
+
+<style scoped>
+h2 {
+  margin-top: 50px;
+}
+
+img {
+  width:200px;
+  height:200px;
+}
+</style>
